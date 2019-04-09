aeeeb081fbd300601285dc0a611f75bd8fcba1f8
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-04-09 13:26:33 +0530

    inserted final algorithm

---
+++ Ford_Fulkerson/Tests/Test01.java
@@ -0,0 +1,10 @@
+package Ford_Fulkerson.Tests;
+
+import java.util.Arrays;
+
+public class Test01 {
+    public static void main(String[] args) {
+        boolean[] arr = new boolean[10];
+        System.out.println(Arrays.toString(arr));
+    }
+}
